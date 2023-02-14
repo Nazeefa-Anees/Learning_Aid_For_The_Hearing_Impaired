28eb9e790171f5e9e0180a053bc99aea9ed1cdfb
NazeefaAnees <nazeefa2030@gmail.com>
2023-02-14 13:12:19 +0530

    splashscreen(sample)

--- learning_aid/src/pages/Splashscreen.jsx
+++ learning_aid/src/pages/Splashscreen.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 export default function Splashscreen() {
   return (
     <div>
-        <div className='bg-teal-400 flex flex-col'>
-            <h1>HIIIIII</h1>
-            <h2>hello</h2>
-        </div>
+        <div className="h-screen bg-gradient-to-br from-blue-500 to-indigo-500 flex items-center justify-center">
+        <button className="w-24 h-16 bg-white rounded-full flex items-center justify-center shadow-lg">
+          Button
+        </button>
+    </div>
     </div>
   )
 }
