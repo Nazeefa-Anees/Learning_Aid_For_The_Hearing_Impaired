167b63a3370ef6365d3a5f0c8843fb7d76fb4075
NazeefaAnees <nazeefa2030@gmail.com>
2023-02-14 12:05:02 +0530

    recreating the front end react project, with vite react, and tailwindcss

---
+++ learning_aid/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Splashscreen from './pages/Splashscreen'
+
+export default function 
+() {
+  return (
+    <div>
+      <Splashscreen/>
+    </div>
+  )
+}
---
+++ learning_aid/src/pages/Splashscreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Splashscreen() {
+  return (
+    <div>
+        <div className='bg-teal-400 flex flex-col'>
+            <h1>HIIIIII</h1>
+            <h2>hello</h2>
+        </div>
+    </div>
+  )
+}
