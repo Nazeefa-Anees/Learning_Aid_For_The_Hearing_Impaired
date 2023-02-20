ff5e5d65d09f6218175e0917d0a2fe689aabea32
Tharanidissanayake <114942687+Tharanidissanayake@users.noreply.github.com>
2023-02-20 13:19:05 +0530

    Learning page (version 1)

--- learning_aid/src/pages/Learning.jsx
+++ learning_aid/src/pages/Learning.jsx
@@ -4,28 +4,32 @@ import homeIcon from '../assets/homeicon.png'
 import '../learning.css'
 
 export default function Learning() {
-    return (
-        <div className="relative w-full h-full">
-          {/* Background Image */}
-          <div className="bg-[url('./assets/learning.png')]absolute w-1440 h-1024 left-0 top-0 bg-cover bg-center"></div>
-    
-          {/* Alphabet Box */}
-          <div className="box bg-yellow-400 rounded-3xl border-black">
-          <h1 className='font-custom text-8xl text-center absolute right-8 mt-8 whitespace-pre-wrap break-words'>
-            ixy,<br/>fydavsh</h1>
-            </div>
-    
-          {/* Number Box */}
-          <div className="box bg-yellow-400 rounded-3xl border-black">
-          <h1 className='font-custom text-8xl text-center absolute right-8 mt-8 whitespace-pre-wrap break-words'>
-            wxl</h1>
-            </div>
-    
-          {/* Back Arrow */}
-          <div className="absolute left-0 right-90.21% top-0 bottom-84.77% bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${backArrow})` }}></div>
-    
-          {/* Home Icon */}
-          <div className="absolute w-157 h-162 left-1259 top-24 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${homeIcon})`}}></div>
-        </div>
-      );
+  return (
+    <div className="relative w-full h-full">
+      {/* Background Image */}
+      <div className="bg-url absolute w-1440 h-1024 left-0 top-0 bg-cover bg-center" style={{ backgroundImage: `url('./assets/learning.png')` }}>
+
+      </div>
+
+      {/* Alphabet Box */}
+      <div className="alphabet-box bg-yellow-400 rounded-3xl border-black absolute top-24 right-8">
+        <h1 className='font-custom text-8xl text-center mt-8 whitespace-pre-wrap'>
+          ixy,<br/>fydavsh
+        </h1>
+      </div>
+
+      {/* Number Box */}
+      <div className="number-box bg-yellow-400 rounded-3xl border-black absolute top-24 left-8">
+        <h1 className='font-custom text-8xl text-center mt-8 whitespace-pre-wrap'>
+          wxl
+        </h1>
+      </div>
+
+       {/* Back Arrow */}
+      <div className="absolute w-12 h-12 left-0 top-0 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${backArrow})`, width: '48px', height: '48px' }}></div>
+
+      {/* Home Icon */}
+      <div className="absolute w-157 h-162 left-1259 top-24 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${homeIcon})`, backgroundSize: 'cover' }}></div>
+    </div>
+  );
 }
