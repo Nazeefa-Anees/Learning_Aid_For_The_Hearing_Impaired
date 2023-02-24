a047b4b15205d96f922c72c765626c5a8903977f
Tharani Dissanayake <tharanijay2@gmail.com>
2023-02-24 14:41:32 +0530

    Learning page and css file updated

--- learning_aid/src/pages/Learning.jsx
+++ learning_aid/src/pages/Learning.jsx
@@ -14,7 +14,7 @@ export default function Learning() {
   
         {/* Alphabet Box */}
         <div className="alphabet-box">
-          <h1 className='alphabet-text font-custom text-8xl text-center mt-8  whitespace-pre-wrap absolute left-10 top-10'>
+          <h1 className='alphabet-text font-custom text-8xl text-center mt-8  whitespace-pre-wrap absolute left-10 top-0'>
             isxy,<br/>fydavsh
           </h1>
         </div>
@@ -30,8 +30,8 @@ export default function Learning() {
         <div className="absolute w-12 h-12 left-0 top-0 bg-no-repeat bg-cover bg-center"
         style={{
           backgroundImage: `url(${backArrow})`,
-          width: "300px",
-          height: "300px",
+          width: "5.5%",
+          height: "10%",
         }}
       ></div>
   
@@ -39,8 +39,8 @@ export default function Learning() {
         <div className="absolute w-12 h-12 right-0 top-0 bg-no-repeat bg-cover bg-center" 
         style={{ 
           backgroundImage: `url(${homeIcon})`, 
-          width: "300px",
-          height: "300px", 
+          width: "5.5%",
+          height: "10%", 
           }}
           ></div>
       </div>
