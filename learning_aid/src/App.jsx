a9671eab88ef3ed7c26a99de27aeb877d6b14fab
NazeefaAnees <nazeefa2030@gmail.com>
2023-02-26 13:11:51 +0530

    update app.jsx

--- learning_aid/src/App.jsx
+++ learning_aid/src/App.jsx
@@ -1,5 +1,11 @@
 
 import * as React from 'react';
+<<<<<<< Updated upstream
+=======
+import { Routes, Route } from 'react-router-dom';
+import Splashscreen from './pages/Splashscreen';
+import Questionnaire from './pages/Questionnaire';
+>>>>>>> Stashed changes
 
 import Learning from './pages/Learning';
 import Quiz from './pages/Quiz';
@@ -8,7 +14,15 @@ export default function
 () {
   return (
     <div>
+<<<<<<< Updated upstream
       <Quiz/>
+=======
+      <Routes>
+        <Route path="/" element={<Splashscreen />} />
+        <Route path="questionnaire" element={<Questionnaire />} />
+      </Routes>
+      
+>>>>>>> Stashed changes
     </div>
   )
 }
