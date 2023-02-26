27fb5862ddda8a717e67bb1eae7f3b085a1618b7
Tharani Dissanayake <tharanijay2@gmail.com>
2023-02-26 08:38:44 +0530

    LearningSelection1,LearningSelection2 and LearningSElection css file Updated

--- learning_aid/src/pages/LearningSelection1.jsx
+++ learning_aid/src/pages/LearningSelection1.jsx
@@ -8,59 +8,61 @@ export default function Learning() {
     return (
       <div className="relative w-full h-full">
         {/* Background Image */}
-        <div className="bg-[url('./assets/learning.jpg')] bg-cover bg-fixed bg-center h-screen ">
+        <div className="bg-[url('./assets/quiz.jpg')] bg-cover bg-fixed bg-center h-screen ">
   
         </div>
 
         <div className="container mx-auto">
             <div className="flex flex-wrap">
-                <Box left={400} top={618} />
-                <Box left={1050} top={618} />
-                <Box left={1690} top={618} />
-                <Box left={2320} top={618} />
-                <Box left={2970} top={618} />
-                <Box left={3580} top={618} />
-                <Box left={4190} top={618} />
-                <Box left={4800} top={618} />
-                <Box left={5400} top={618} />
-                <Box left={400} top={1320} />
-                <Box left={1050} top={1320} />
-                <Box left={1690} top={1320} />
-                <Box left={2320} top={1320} />
-                <Box left={2970} top={1320} />
-                <Box left={3580} top={1320} />
-                <Box left={4190} top={1320} />
-                <Box left={4800} top={1320} />
-                <Box left={5400} top={1320} />
-                <Box left={400} top={2000} />
-                <Box left={1050} top={2000} />
-                <Box left={1690} top={2000} />
-                <Box left={2320} top={2000} />
-                <Box left={2970} top={2000} />
-                <Box left={3580} top={2000} />
-                <Box left={4190} top={2000} />
-                <Box left={4800} top={2000} />
-                <Box left={5400} top={2000} />
+                <Box left={80} top={140} />
+                <Box left={230} top={140} />
+                <Box left={390} top={140} />
+                <Box left={550} top={140} />
+                <Box left={710} top={140} />
+                <Box left={870} top={140} />
+                <Box left={1030} top={140} />
+                <Box left={1190} top={140} />
+                <Box left={1350} top={140} />
+                <Box left={80} top={340} />
+                <Box left={230} top={340} />
+                <Box left={390} top={340} />
+                <Box left={550} top={340} />
+                <Box left={710} top={340} />
+                <Box left={870} top={340} />
+                <Box left={1030} top={340} />
+                <Box left={1190} top={340} />
+                <Box left={1350} top={340} />
+                <Box left={80} top={540} />
+                <Box left={230} top={540} />
+                <Box left={390} top={540} />
+                <Box left={550} top={540} />
+                <Box left={710} top={540} />
+                <Box left={870} top={540} />
+                <Box left={1030} top={540} />
+                <Box left={1190} top={540} />
+                <Box left={1350} top={540} />
             </div>
         </div>
   
-         {/* Back Arrow */}
-         <div className="absolute w-12 h-12 left-0 top-0 bg-no-repeat bg-cover bg-center"
+        {/* Back Arrow */}
+      <div
+        className="absolute w-12 h-12 left-0 top-0 bg-no-repeat bg-cover bg-center"
         style={{
           backgroundImage: `url(${backArrow})`,
-          width: "300px",
-          height: "300px",
+          width: '5.5%',
+          height: '10%'
         }}
       ></div>
-  
-        {/* Home Icon */}
-        <div className="absolute w-12 h-12 right-0 top-0 bg-no-repeat bg-cover bg-center" 
-        style={{ 
-          backgroundImage: `url(${homeIcon})`, 
-          width: "300px",
-          height: "300px", 
-          }}
-          ></div>
+
+      {/* Home Icon */}
+      <div
+        className="absolute w-12 h-12 right-0 top-0 bg-no-repeat bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${homeIcon})`,
+          width: '5.5%',
+          height: '10%'
+        }}
+      ></div>
       </div>
     );
   
--- learning_aid/src/pages/LearningSelection2.jsx
+++ learning_aid/src/pages/LearningSelection2.jsx
@@ -8,41 +8,43 @@ export default function Learning() {
     return (
       <div className="relative w-full h-full">
         {/* Background Image */}
-        <div className="bg-[url('./assets/learning.jpg')] bg-cover bg-fixed bg-center h-screen ">
+        <div className="bg-[url('./assets/quiz.jpg')] bg-cover bg-fixed bg-center h-screen ">
   
         </div>
 
         <div className="container mx-auto">
             <div className="flex flex-wrap">
-                <Box left={1500} top={618} />
-                <Box left={2700} top={618} />
-                <Box left={3890} top={618} />
-                <Box left={1500} top={1320} />
-                <Box left={2700} top={1320} />
-                <Box left={3890} top={1320} />
-                <Box left={1500} top={2000} />
-                <Box left={2700} top={2000} />
-                <Box left={3890} top={2000} />
+                <Box left={360} top={140} />
+                <Box left={660} top={140} />
+                <Box left={960} top={140} />
+                <Box left={360} top={340} />
+                <Box left={660} top={340} />
+                <Box left={960} top={340} />
+                <Box left={360} top={540} />
+                <Box left={660} top={540} />
+                <Box left={960} top={540} />
             </div>
         </div>
   
         {/* Back Arrow */}
-        <div className="absolute w-12 h-12 left-0 top-0 bg-no-repeat bg-cover bg-center"
+      <div
+        className="absolute w-12 h-12 left-0 top-0 bg-no-repeat bg-cover bg-center"
         style={{
           backgroundImage: `url(${backArrow})`,
-          width: "300px",
-          height: "300px",
+          width: '5.5%',
+          height: '10%'
         }}
       ></div>
-  
-        {/* Home Icon */}
-        <div className="absolute w-157 h-162 right-0 top-0 bg-no-repeat bg-cover bg-center" 
-        style={{ 
-          backgroundImage: `url(${homeIcon})`, 
-          width: "300px",
-          height: "300px", 
-          }}
-          ></div>
+
+      {/* Home Icon */}
+      <div
+        className="absolute w-12 h-12 right-0 top-0 bg-no-repeat bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${homeIcon})`,
+          width: '5.5%',
+          height: '10%'
+        }}
+      ></div>
       </div>
     );
   
