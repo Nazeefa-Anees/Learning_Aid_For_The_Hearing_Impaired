d38471ac0565c4334e56acbc0a98ae6afbba64cc
sahas_v <sahasvinduwara2k02@gmail.com>
2023-03-18 18:00:56 +0530

    Update Letter1.jsx

--- learning_aid/src/pages/Letter1.jsx
+++ learning_aid/src/pages/Letter1.jsx
@@ -1,61 +1,63 @@
-import React, { useRef, useEffect } from "react";
-import backArrow from "../assets/backarrow.png";
-import next from "../assets/next.png";
-import one from "../assets//dataset_icons/letters/1.jpg";
-import homeIcon from "../assets/homeicon.png";
-import "./Letter.css";
-import { Link } from "react-router-dom";
-
-import { Hands } from "@mediapipe/hands";
-import * as tf from "@tensorflow/tfjs";
+import React, { useRef, useEffect } from 'react';
+import Hands from '@mediapipe/hands';
+import backArrow from '../assets/backarrow.png';
+import next from '../assets/next.png';
+import one from '../assets//dataset_icons/letters/1.jpg';
+import homeIcon from'../assets/homeicon.png';
+import { Link } from 'react-router-dom';
 
 export default function Letter1() {
-  const hands = new Hands({
-    locateFile: (file) => {
-      return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-    },
-  });
-
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    const video = videoRef.current;
+    async function setup() {
+      const hands = new Hands({ locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}` });
+      await hands.initialize();
 
-    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-      video.srcObject = stream;
+      const video = videoRef.current;
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
 
-      hands.setOptions({
-        maxNumHands: 1,
-        minDetectionConfidence: 0.8,
-        minTrackingConfidence: 0.5,
-      });
+      const draw = () => {
+        hands.send({ image: video }).then((results) => {
+          context.clearRect(0, 0, canvas.width, canvas.height);
+          context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-      hands.onResults((results) => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext("2d");
-        context.clearRect(0, 0, canvas.width, canvas.height);
-      
-        results.multiHandLandmarks.forEach((handLandmarks) => {
-          hands.drawConnectors(context, handLandmarks, Hands.HAND_CONNECTIONS);
-          hands.drawLandmarks(context, handLandmarks, Hands.HAND_CONNECTIONS);
-      
-          const prediction = predictGesture(handLandmarks);
-          console.log(prediction);
+          if (results.multiHandLandmarks) {
+            for (const landmarks of results.multiHandLandmarks) {
+              for (const [index, landmark] of landmarks.entries()) {
+                const x = landmark.x * canvas.width;
+                const y = landmark.y * canvas.height;
+                context.beginPath();
+                context.arc(x, y, 5, 0, 2 * Math.PI);
+                context.fillStyle = 'blue';
+                context.fill();
+
+                if (index > 0) {
+                  const prevLandmark = landmarks[index - 1];
+                  const prevX = prevLandmark.x * canvas.width;
+                  const prevY = prevLandmark.y * canvas.height;
+                  context.beginPath();
+                  context.moveTo(prevX, prevY);
+                  context.lineTo(x, y);
+                  context.strokeStyle = 'blue';
+                  context.lineWidth = 2;
+                  context.stroke();
+                }
+              }
+            }
+          }
+
+          requestAnimationFrame(draw);
         });
-      });
-      
-      hands.initialize();
-    });
-  }, []);
+      };
 
-  async function predictGesture(handLandmarks) {
-    const model = await tf.loadLayersModel("../assets/models/model_Letters/tfjs_model/model.json");
-    const tensor = tf.tensor(handLandmarks);
-    const prediction = model.predict(tensor);
-    const output = prediction.arraySync()[0];
-    return output;
-  }  
+      draw();
+    }
+
+    setup();
+  }, []);
 
   return (
     <div className="page-container">
@@ -64,25 +66,13 @@ export default function Letter1() {
 
       {/*Text*/}
       <div className="box1">
-        <h1 className="font-custom text-9xl text-center mt-8  whitespace-pre-wrap absolute left-16 top-0">
-          w{" "}
-        </h1>
+        <h1 className='font-custom text-9xl text-center mt-8  whitespace-pre-wrap absolute left-16 top-0'>w </h1>
       </div>
 
       {/* Camera */}
       <div className="box2">
-        <video 
-          ref={videoRef}
-          className="video"
-          autoPlay 
-          muted
-        />
-        <canvas
-          ref={canvasRef}
-          className="canvas"
-          width={640}
-          height={480}
-        />
+        <video ref={videoRef} autoPlay muted className="w-full h-full object-contain" />
+        <canvas ref={canvasRef} className="w-full h-full absolute top-0 left-0" />
       </div>
 
       {/*Image*/}
@@ -91,48 +81,48 @@ export default function Letter1() {
       </div>
 
       {/* Back Arrow */}
-      <Link to="/letters">
+      <Link to='/letters'>
         <button>
           <div
             className="absolute w-12 h-12 left-0 top-0 bg-no-repeat bg-cover bg-center"
             style={{
               backgroundImage: `url(${backArrow})`,
-              width: "5.5%",
-              height: "10%",
+              width: '5.5%',
+              height: '10%'
             }}
           ></div>
         </button>
       </Link>
 
       {/* Home Icon */}
-      <Link to="/home">
-        <button>
-          <div
-            className="absolute w-12 h-12 right-0 top-0 bg-no-repeat bg-cover bg-center"
-            style={{
-              backgroundImage: `url(${homeIcon})`,
-              width: "5.5%",
-              height: "10%",
-            }}
-          ></div>
-        </button>
+      <Link to='/home'>
+      <button>
+      <div
+        className="absolute w-12 h-12 right-0 top-0 bg-no-repeat bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${homeIcon})`,
+          width: '5.5%',
+          height: '10%'
+        }}
+      ></div>
+      </button>
       </Link>
 
       {/* Next */}
-      <Link to="/letter2">
-        <button>
-          <div
-            className="absolute w-12 h-12 bg-no-repeat bg-cover bg-center"
-            style={{
-              backgroundImage: `url(${next})`,
-              width: "5.5%",
-              height: "10%",
-              right: 20,
-              top: "50%",
-              transform: "translateY(-50%)",
-            }}
-          ></div>
-        </button>
+      <Link to='/letter2'>
+      <button>
+      <div
+        className="absolute w-12 h-12 bg-no-repeat bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${next})`,
+          width: "5.5%",
+          height: "10%",
+          right: 20,
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+      ></div>
+      </button>
       </Link>
     </div>
   );
