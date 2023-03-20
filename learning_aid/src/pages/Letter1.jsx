12f1761af13e8926ecdda38d687b39c0ede1108c
sahas_v <sahasvinduwara2k02@gmail.com>
2023-03-20 10:21:00 +0530

    Revert "blur and countdown in letter 1"
    This reverts commit 29a035973403236e4d81096275cb3a4d11acf99f.

--- learning_aid/src/pages/Letter1.jsx
+++ learning_aid/src/pages/Letter1.jsx
@@ -9,23 +9,6 @@ import HandTracking from '../components/HandTracking';
 
 export default function Letter1() {
   const [showBlur, setShowBlur] = useState(true);
-  const [countdown, setCountdown] = useState(3);
-
-  useEffect(() => {
-    let countdownInterval;
-
-    if (countdown > 0 && !showBlur) {
-      countdownInterval = setInterval(() => {
-        setCountdown(countdown - 1);
-      }, 1000);
-    }
-
-    if (countdown === 0) {
-      setShowBlur(false);
-    }
-
-    return () => clearInterval(countdownInterval);
-  }, [countdown, showBlur]);
 
   return (
     <div className="page-container">
@@ -43,12 +26,6 @@ export default function Letter1() {
       <div className="box2">
         <HandTracking />
         {showBlur && <div className="blurback"></div>}
-        {!showBlur && countdown > 0 && (
-          <div className="countdown">
-            <div className="countdown-text">{countdown}</div>
-          </div>
-        )}
-        {!showBlur && countdown === 0}
         {showBlur && (
           <div className="button">
             <button className="button-text" onClick={() => setShowBlur(false)}>Click to Try</button>
