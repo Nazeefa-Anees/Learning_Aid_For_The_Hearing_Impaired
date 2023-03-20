7a55e144d6c2625592deea5c3a42194e13cf74fd
sahas_v <sahasvinduwara2k02@gmail.com>
2023-03-20 16:54:57 +0530

    letter 1 works

--- learning_aid/src/pages/Letter1.jsx
+++ learning_aid/src/pages/Letter1.jsx
@@ -1,3 +1,55 @@
+{/*}
+import React, { useRef, useEffect } from "react";
+import backArrow from "../assets/backarrow.png";
+import next from "../assets/next.png";
+import one from "../assets//dataset_icons/letters/1.jpg";
+import homeIcon from "../assets/homeicon.png";
+import "./Letter.css";
+import { Link } from "react-router-dom";
+
+import { Hands } from "@mediapipe/hands";
+import * as tf from "@tensorflow/tfjs";
+
+export default function Letter1() {
+  const hands = new Hands({
+    locateFile: (file) => {
+      return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+    },
+  });
+
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+      video.srcObject = stream;
+
+      hands.setOptions({
+        maxNumHands: 1,
+        minDetectionConfidence: 0.8,
+        minTrackingConfidence: 0.5,
+      });
+
+      hands.onResults((results) => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+        context.clearRect(0, 0, canvas.width, canvas.height);
+      
+        results.multiHandLandmarks.forEach((handLandmarks) => {
+          hands.drawConnectors(context, handLandmarks, Hands.HAND_CONNECTIONS);
+          hands.drawLandmarks(context, handLandmarks, Hands.HAND_CONNECTIONS);
+      
+          const prediction = predictGesture(handLandmarks);
+          console.log(prediction);
+        });
+      });
+      
+      hands.initialize();
+    });
+  }, []);
+*/}
 import React, { useState, useEffect } from "react";
 import backArrow from '../assets/backarrow.png';
 import next from '../assets/next.png';
@@ -8,15 +60,33 @@ import { Link } from 'react-router-dom';
 import HandTracking from '../components/HandTracking';
 
 export default function Letter1() {
-  const [showBlur, setShowBlur] = useState(true);
-
+{/*}
   async function predictGesture(handLandmarks) {
     const model = await tf.loadLayersModel("../assets/models/model_Letters/tfjs_model/model.json");
     const tensor = tf.tensor(handLandmarks);
     const prediction = model.predict(tensor);
     const output = prediction.arraySync()[0];
     return output;
-  }  
+  }
+*/}
+  const [showBlur, setShowBlur] = useState(true);
+  const [countdown, setCountdown] = useState(3);
+
+  useEffect(() => {
+    let countdownInterval;
+
+    if (countdown > 0 && !showBlur) {
+      countdownInterval = setInterval(() => {
+        setCountdown(countdown - 1);
+      }, 1000);
+    }
+
+    if (countdown === 0) {
+      setShowBlur(false);
+    }
+
+    return () => clearInterval(countdownInterval);
+  }, [countdown, showBlur]);
 
   return (
     <div className="page-container">
@@ -34,6 +104,12 @@ export default function Letter1() {
       <div className="box2">
         <HandTracking />
         {showBlur && <div className="blurback"></div>}
+        {!showBlur && countdown > 0 && (
+          <div className="countdown">
+            <div className="countdown-text">{countdown}</div>
+          </div>
+        )}
+        {!showBlur && countdown === 0}
         {showBlur && (
           <div className="button">
             <button className="button-text" onClick={() => setShowBlur(false)}>Click to Try</button>
