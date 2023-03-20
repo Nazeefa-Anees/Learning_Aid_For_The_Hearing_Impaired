a14e59e5b8bf94d1d87b1cee693f09e09a52dac1
Tharani Dissanayake <tharanijay2@gmail.com>
2023-03-20 17:30:15 +0530

    Q1 Page

--- learning_aid/src/pages/Q1.jsx
+++ learning_aid/src/pages/Q1.jsx
@@ -1,86 +1,18 @@
 import React, { useRef, useEffect, useState } from "react";
-import * as tf from '@tensorflow/tfjs';
-import * as handpose from '@mediapipe/hands';
-// import '@mediapipe/hands/dist/hands.css';
-
-
 import Webcam from "react-webcam";
 import homeIcon from "../assets/homeicon.png";
 import next from "../assets/next.png";
 import "./Question.css";
 import { Link } from "react-router-dom";
-
 export default function Q1() {
   const webcamRef = useRef();
   const canvasRef = useRef();
-  const handsRef = useRef();
-  const modelRef = useRef();
-
-  useEffect(() => {
-    async function loadModel() {
-      modelRef.current = await tf.loadLayersModel("../assets/models/model_Numbers/tfjs_model/model.json");
-    }
-
-    async function setupHandPose() {
-      await handpose.load();
-      handsRef.current = new handpose.Hands({
-        maxNumHands: 1,
-        detectionConfidence: 0.8,
-        trackingConfidence: 0.8,
-      });
-    }
-
-    loadModel();
-    setupHandPose();
-  }, []);
-
-  useEffect(() => {
-    async function startHandPoseEstimation() {
-      const video = webcamRef.current.video;
-      const canvas = canvasRef.current;
-      const hands = handsRef.current;
-      const model = modelRef.current;
-
-      if (video && canvas && hands && model) {
-        hands.onResults(async (results) => {
-          const landmarks = results?.multiHandLandmarks[0];
-          if (landmarks) {
-            const tensor = tf.tensor(landmarks.flat());
-            const prediction = model.predict(tensor.expandDims());
-            const gesture = await prediction.argMax().data();
-
-            // Provide feedback to the user based on the gesture prediction
-            // ...
-
-            tf.dispose([tensor, prediction]);
-          }
-
-          hands.drawConnectors(canvas, results?.multiHandLandmarks[0], handpose.HAND_CONNECTIONS);
-          hands.drawLandmarks(canvas, results?.multiHandLandmarks[0], handpose.HAND_CONNECTIONS);
-        });
-
-        const canvasContext = canvas.getContext('2d');
-        const renderFrame = () => {
-          canvasContext.drawImage(video, 0, 0, canvas.width, canvas.height);
-          hands.send({ image: video });
-          requestAnimationFrame(renderFrame);
-        };
-
-        renderFrame();
-      }
-    }
-
-    startHandPoseEstimation();
-  }, [webcamRef, canvasRef, handsRef, modelRef]);
-
   const [capturedImage, setCapturedImage] = useState(null);
   const [prediction, setPrediction] = useState(null);
-
   const handleCapture = () => {
     const imageSrc = webcamRef.current.getScreenshot();
     setCapturedImage(imageSrc);
   };
-
   const handleSendImage = (event) => {
     event.preventDefault();
     if (capturedImage) {
@@ -101,7 +33,6 @@ export default function Q1() {
     <div className="page-container">
       {/* Background Image */}
       <div className="bg-[url(./assets/quiz.jpg)] bg-cover bg-center min-h-screen"></div>
-
       {/*Text*/}
       <h1
         className="font-custom text-9xl text-center mt-8 absolute top-40"
@@ -116,7 +47,6 @@ export default function Q1() {
         <br />
         fmkajkak
       </h1>
-
       {/* Camera */}
       <div style={{
   position: "absolute",
@@ -156,7 +86,6 @@ export default function Q1() {
 >
   .kak
 </button>
-
 <button 
   onClick={handleSendImage} 
   className="font-custom bg-yellow-500 hover:bg-yellow-200 text-black font-bold py-2 px-4 rounded"
