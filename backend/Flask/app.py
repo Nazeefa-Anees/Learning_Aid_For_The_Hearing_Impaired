52e4f43207349f1ad064a740050c6ab657e07dfb
Tharani Dissanayake <tharanijay2@gmail.com>
2023-03-23 20:29:17 +0530

    Flask Folder Updated

--- backend/Flask/app.py
+++ backend/Flask/app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask,redirect,url_for,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello, world!'
+def welcome():
+    return render_template('index.html')
+
+@app.route('/success/<int:score>')
+def success(score):
+    return 'The marks are'+ str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return 'The marks are'+ str(score)
+
+
+#result checker
+@app.route('/results/<int:marks>')
+def results(marks):
+    result = ""
+    if marks<50:
+        result = 'fail'
+    else:
+        result ='success'
+    return redirect(url_for(result,score=marks))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
