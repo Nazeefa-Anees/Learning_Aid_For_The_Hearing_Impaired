3fa46b42d14bd1f4db4b14c28980c71874059b31
sahas_v <sahasvinduwara2k02@gmail.com>
2023-04-01 11:37:34 +0530

    testing hands

---
+++ backend/Flask/static/js/handsTest.js
@@ -0,0 +1,92 @@
+const mpHands = window;
+const drawingUtils = window;
+const controls = window;
+
+// Our input frames will come from here.
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const controlsElement = document.getElementsByClassName('control-panel')[0];
+const canvasCtx = canvasElement.getContext('2d');
+const config = { locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${mpHands.VERSION}/${file}`;
+    } };
+
+function onResults(results) {
+    // Draw the overlays.
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+    if (results.multiHandLandmarks && results.multiHandedness) {
+        for (let index = 0; index < results.multiHandLandmarks.length; index++) {
+            const classification = results.multiHandedness[index];
+            const isRightHand = classification.label === 'Right';
+            const landmarks = results.multiHandLandmarks[index];
+            drawingUtils.drawConnectors(canvasCtx, landmarks, mpHands.HAND_CONNECTIONS, { 
+                color: isRightHand ? '#00FF00' : '#FF0000' });
+            drawingUtils.drawLandmarks(canvasCtx, landmarks, {
+                color: isRightHand ? '#00FF00' : '#FF0000',
+                fillColor: isRightHand ? '#FF0000' : '#00FF00',
+                radius: (data) => {
+                    return drawingUtils.lerp(data.from.z, -0.15, .1, 10, 1);
+                }
+            });
+        }
+    }
+
+    canvasCtx.restore();
+    if (results.multiHandWorldLandmarks) {
+        // We only get to call updateLandmarks once, so we need to cook the data to
+        // fit. The landmarks just merge, but the connections need to be offset.
+        const landmarks = results.multiHandWorldLandmarks.reduce((prev, current) => [...prev, ...current], []);
+        const colors = [];
+        let connections = [];
+        for (let loop = 0; loop < results.multiHandWorldLandmarks.length; ++loop) {
+            const offset = loop * mpHands.HAND_CONNECTIONS.length;
+            const offsetConnections = mpHands.HAND_CONNECTIONS.map((connection) => [connection[0] + offset, connection[1] + offset]);
+            connections = connections.concat(offsetConnections);
+            const classification = results.multiHandedness[loop];
+            colors.push({
+                list: offsetConnections.map((unused, i) => i + offset),
+                color: classification.label,
+            });
+        }
+    }
+}
+
+const hands = new mpHands.Hands(config);
+hands.onResults(onResults);
+
+// Present a control panel through which the user can manipulate the solution
+// options.
+new controls.ControlPanel(controlsElement, {
+    selfieMode: true, // set selfieMode to true
+    maxNumHands: 1,
+    modelComplexity: 1,
+    minDetectionConfidence: 0.8,
+    minTrackingConfidence: 0.5
+})
+
+.add([
+    new controls.SourcePicker({
+        onFrame: async (input, size) => {
+            const aspect = size.height / size.width;
+            let width, height;
+            if (window.innerWidth > window.innerHeight) {
+                height = window.innerHeight;
+                width = height / aspect;
+            } else {
+                width = window.innerWidth;
+                height = width * aspect;
+            }
+            canvasElement.width = width;
+            canvasElement.height = height;
+            await hands.send({ image: input });
+        },
+    }),
+])
+
+.on(x => {
+    const options = x;
+    videoElement.classList.toggle('selfie', true); // always set to true
+    hands.setOptions(options);
+});
