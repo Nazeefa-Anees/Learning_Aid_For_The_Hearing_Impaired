e5cd7226f36fbbdfb7efa8f39233ac4b98bb4664
Tharani Dissanayake <tharanijay2@gmail.com>
2023-04-07 18:51:53 +0530

    question1, question css and question.js file updated

--- backend/Flask/static/js/question.js
+++ backend/Flask/static/js/question.js
@@ -1,30 +1,39 @@
-function startCamera() {
-    // Hide the button
-    document.getElementById("start-camera").style.display = "none";
+function processImage(imageData, questionIndex) {
+    // ... existing code to process the image and get accuracy ...
     
-    // Display the countdown
-    var countdown = 3;
-    var countdownInterval = setInterval(function() {
-        document.getElementById("countdown").innerHTML = countdown;
-        countdown--;
-        if (countdown < 0) {
-        clearInterval(countdownInterval);
-        document.getElementById("countdown").style.display = "none";
-        document.getElementById("video").style.display = "block";
-        }
-    }, 1000);
-    
-    // Start the camera
-    navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function(stream) {
-        var video = document.getElementById("video");
-        video.srcObject = stream;
-        video.play();
-        })
-        .catch(function(error) {
-        console.log(error);
-        });
+    // calculate score based on accuracy
+    const accuracy = ... // accuracy value obtained from backend
+    let isCorrect = false;
+    if (accuracy > 80) {
+      // answer is considered correct
+      isCorrect = true;
+      totalScore += 1; // increment total score by 1
     }
     
-export default startCamera;
+    // store user's answer for later retrieval
+    questions[questionIndex].userAnswer = isCorrect;
+  }
+
+  // add an event listener to the form submit event
+const form = document.getElementById('quiz-form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
   
+  // calculate total score
+  totalScore = 0; // reset total score
+  for (let i = 0; i < questions.length; i++) {
+    if (questions[i].userAnswer === true) {
+      totalScore += 1; // increment total score for correct answers
+    }
+  }
+  
+  // display feedback message based on total score
+  const feedbackSection = document.getElementById('feedback');
+  const feedbackMessage = document.createElement('p');
+  if (totalScore === questions.length) {
+    feedbackMessage.textContent = `Congratulations! You answered all questions correctly. Your total score is ${totalScore} out of ${questions.length}.`;
+  } else {
+    feedbackMessage.textContent = `You answered ${totalScore} out of ${questions.length} questions correctly. Please review your answers and try again.`;
+  }
+  feedbackSection.appendChild(feedbackMessage);
+});
