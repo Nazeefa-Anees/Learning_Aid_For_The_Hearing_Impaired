31ae80cbf803291ca365d384139cbf78f9a7c4c4
Tharani Dissanayake <tharanijay2@gmail.com>
2023-04-08 22:44:38 +0530

    question.js file updated

--- backend/Flask/static/js/question.js
+++ backend/Flask/static/js/question.js
@@ -1,24 +1,40 @@
 function processImage(imageData, questionIndex) {
-    // ... existing code to process the image and get accuracy ...
-    
-    // calculate score based on accuracy
-    const accuracy = ... // accuracy value obtained from backend
-    let isCorrect = false;
-    if (accuracy > 80) {
-      // answer is considered correct
-      isCorrect = true;
-      totalScore += 1; // increment total score by 1
-    }
-    
-    // store user's answer for later retrieval
-    questions[questionIndex].userAnswer = isCorrect;
-  }
+  // ... existing code to process the image and get accuracy ...
 
-  // add an event listener to the form submit event
+  // make API call to backend to get accuracy
+  fetch('/api/accuracy', {
+    method: 'POST',
+    body: JSON.stringify({ imageData }), // pass image data to backend
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      // get accuracy from response
+      const accuracy = data.accuracy;
+
+      // calculate score based on accuracy
+      let isCorrect = false;
+      if (accuracy > 80) {
+        // answer is considered correct
+        isCorrect = true;
+        totalScore += 1; // increment total score by 1
+      }
+
+      // store user's answer for later retrieval
+      questions[questionIndex].userAnswer = isCorrect;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+// add an event listener to the form submit event
 const form = document.getElementById('quiz-form');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  
+
   // calculate total score
   totalScore = 0; // reset total score
   for (let i = 0; i < questions.length; i++) {
@@ -26,7 +42,7 @@ form.addEventListener('submit', (event) => {
       totalScore += 1; // increment total score for correct answers
     }
   }
-  
+
   // display feedback message based on total score
   const feedbackSection = document.getElementById('feedback');
   const feedbackMessage = document.createElement('p');
